fc03e47b86c17eb2b38de323e8802decd0046da9
Mark Biek <mark.biek@automattic.com>
2022-09-06 10:40:34 -0400

    Initial types

---
+++ src/types.ts
@@ -0,0 +1,36 @@
+export interface Ship {
+	fuel: number;
+	fuel_size: number;
+	cargo_size: number;
+}
+
+export interface Player {
+	name: string;
+	ship: Ship;
+	credits: number;
+	current_location: SolarSystem;
+	missions: Mission[];
+}
+
+export interface Mission {
+	description: string;
+	type: 'trade';
+	amount: number;
+	source: Planet;
+	destination: Planet;
+}
+
+export interface Planet {
+	name: string;
+	description: string;
+	fuel: boolean;
+	trade: boolean;
+	gambling: boolean;
+}
+
+export interface SolarSystem {
+	name: string;
+	description: string;
+	planets: Planet[] | null;
+	connected_systems: SolarSystem[];
+}
