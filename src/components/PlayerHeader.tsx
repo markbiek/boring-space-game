5142cfca19bf1f72b699a57309df8f9092968afe
Mark Biek <mark.biek@automattic.com>
2022-09-19 16:50:53 -0400

    Show cargo details in player header

--- src/components/PlayerHeader.tsx
+++ src/components/PlayerHeader.tsx
@@ -1,22 +1,61 @@
+import { useState } from 'react';
+
 import { currentView } from '../store/reducers/view';
 import {
 	playerName,
 	playerCredits,
 	playerHopsRemaining,
 	playerAvailableCargo,
+	playerShip,
 } from '../store/reducers/player';
 import { useAppSelector } from '../store/hooks';
 
 export default function PlayerHeader() {
+	const [showCargo, setShowCargo] = useState(false);
 	const view = useAppSelector(currentView);
 	const player = useAppSelector(playerName);
 	const credits = useAppSelector(playerCredits);
 	const hops = useAppSelector(playerHopsRemaining);
 	const cargoSize = useAppSelector(playerAvailableCargo);
+	const ship = useAppSelector(playerShip);
 
 	if (view === 'start') {
 		return null;
 	}
+
+	const cargoDetails = () => {
+		const { cargo } = ship;
+		const details: {
+			[index: string]: number;
+		} = {};
+
+		for (const cargoItem of cargo) {
+			const {
+				item: { name, weight },
+			} = cargoItem;
+
+			if (!details.hasOwnProperty(name)) {
+				details[name] = 0;
+			}
+
+			details[name] += weight;
+		}
+
+		return (
+			<table className="cargo-details">
+				<tbody>
+					{Object.keys(details).map((key) => {
+						return (
+							<tr key={key}>
+								<td>{key}</td>
+								<td>{details[key]}kg</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		);
+	};
 
 	return (
 		<header className="player-info">
@@ -28,6 +67,14 @@ export default function PlayerHeader() {
 				<li className={`${hops > 0 ? '' : 'low-fuel'}`}>Hops: {hops}</li>
 				<li>Available Cargo: {cargoSize}</li>
 			</ul>
+			<button
+				onClick={(e) => {
+					setShowCargo(!showCargo);
+				}}
+			>
+				{showCargo ? 'Hide Cargo' : 'Show Cargo'}
+			</button>
+			{showCargo && cargoDetails()}
 		</header>
 	);
 }
